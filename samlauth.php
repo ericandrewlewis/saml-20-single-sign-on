a51d048f90d905013503cc5baf170a16dcd0a51a
ktbartholomew <ktbartholomew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-08 13:56:12 +0000

    updated version numbers to 0.8.6
    git-svn-id: http://plugins.svn.wordpress.org/saml-20-single-sign-on/tags/0.8.6@709891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- samlauth.php
+++ samlauth.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: SAML 2.0 Single Sign-On
-Version: 0.8.5
+Version: 0.8.6
 Plugin URI: http://keithbartholomew.com
 Description: Authenticate users using <a href="http://rnd.feide.no/simplesamlphp">simpleSAMLphp</a>.
 Author: Keith Bartholomew
