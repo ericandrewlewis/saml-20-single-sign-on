7f25e6e94c86e670142c79f644230d755c6518b1
ktbartholomew <ktbartholomew@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-03 05:13:56 +0000

    Trying an additional commit to get WP.org to recognize the new version
    git-svn-id: http://plugins.svn.wordpress.org/saml-20-single-sign-on/trunk@850297 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- samlauth.php
+++ samlauth.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: SAML 2.0 Single Sign-On
-Version: 0.9.1
+Version: 0.9.2
 Plugin URI: http://keithbartholomew.com
 Description: Authenticate users using <a href="http://rnd.feide.no/simplesamlphp">simpleSAMLphp</a>.
 Author: Keith Bartholomew
