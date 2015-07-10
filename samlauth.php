bd584495ffe082701f3eff3f5dfaee702d76159e
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-07-10 17:37:46 -0400

    Set SAMLAUTH_URL properly for usage in mu-plugins

--- samlauth.php
+++ samlauth.php
@@ -12,7 +12,7 @@ $upload_dir = wp_upload_dir();
 define('SAMLAUTH_CONF', $upload_dir['basedir'] . '/saml-20-single-sign-on/etc');
 define('SAMLAUTH_CONF_URL', $upload_dir['baseurl'] . '/saml-20-single-sign-on/etc');
 define('SAMLAUTH_ROOT',dirname(__FILE__));
-define('SAMLAUTH_URL',plugins_url() . '/' . basename( dirname(__FILE__) ) );
+define('SAMLAUTH_URL', plugins_url( '', __FILE__ ));
 define('SAMLAUTH_MD_URL', constant('SAMLAUTH_URL') . '/saml/www/module.php/saml/sp/metadata.php/' . get_current_blog_id() );
 
 // Things needed everywhere
